e152070251278b6381491cb15a03d6c183f94802
kot <bafomet2016vitt@gmail.com>
2023-08-06 22:49:09 +0300

    fix

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from catalog.views import index, sidebar_css, sidebar_js, home, contacts, messages
+from catalog.views import home, contacts, messages
 
 urlpatterns = [
-    path('index.html', index),
     path('', home),
-    path('messages.html', messages),
-    path('contacts.html', contacts),
-    path('sidebars.css', sidebar_css),
-    path('sidebars.js', sidebar_js)
+    path('messages/', messages),
+    path('contacts/', contacts),
 ]
