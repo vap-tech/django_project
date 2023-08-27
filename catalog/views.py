28824bc8ef5b7de8d69e54ccc50ae143a0f9dbf4
kot <bafomet2016vitt@gmail.com>
2023-08-27 16:32:26 +0300

    refactor CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,39 +1,42 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView
 
-from catalog.function import construct_html, JsonC
-from catalog.models import Product
-
+from catalog.models import Product, Category, Feedback
 
 # Create your views here.
 
 
-def home(request):
-    product_list = Product.objects.all()
-    context = {
-        'object_list': product_list
-    }
-    return render(request, 'catalog/home.html', context)
+class CategoryListView(ListView):
+    model = Category
 
 
-def product(request, product_pk):
-    product_list = Product.objects.get(pk=product_pk)
-    context = {
-        'object': product_list
-    }
-    return render(request, 'catalog/product.html', context)
+class ProductListView(ListView):
+    model = Product
 
 
-def messages(request):
-    return render(request, 'catalog/messages.html')
+class FeedbackListView(ListView):
+    model = Feedback
 
 
-def contacts(request):
-    construct_html()
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        tel = request.POST.get('tel')
-        message = request.POST.get('text')
-        JsonC().to_file([{"name": name, "tel": tel, "email": email, "text": message}])
-        construct_html()
-    return render(request, 'catalog/contacts.html')
+class ProductDetailView(DetailView):
+    model = Product
+
+
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = ('name', 'description')
+    success_url = reverse_lazy('catalog:category')
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = (
+        'name', 'description', 'preview', 'category', 'price', 'created_date', 'last_modified_date',
+    )
+    success_url = reverse_lazy('catalog:product')
+
+
+class FeedbackCreateView(CreateView):
+    model = Feedback
+    fields = ('name', 'phone', 'email', 'message')
+    success_url = reverse_lazy('catalog:category')
