5702891e199609eafa1eb97d3ebb3d0e12c44536
kot <bafomet2016vitt@gmail.com>
2023-08-28 13:15:08 +0300

    blog add

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from catalog.views import CategoryListView, CategoryCreateView, ProductListView, \
-    ProductCreateView, ProductDetailView, FeedbackCreateView, FeedbackListView
+    ProductCreateView, ProductDetailView, FeedbackCreateView, FeedbackListView, BlogListView, \
+    BlogCreateView, BlogDetailView, BlogUpdateView
 
 from catalog.apps import CatalogConfig
 
@@ -11,10 +12,15 @@ urlpatterns = [
     path('', CategoryListView.as_view(), name='category'),
     path('product/', ProductListView.as_view(), name='product'),
     path('messages/', FeedbackListView.as_view(), name='messages'),
+    path('blog/', BlogListView.as_view(), name='blog'),
 
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('blog/<str:slug>/', BlogDetailView.as_view(), name='blog-detail'),
+    path('blog/<str:slug>/update/', BlogUpdateView.as_view(), name='blog-update'),
+
 
     path('category-create/', CategoryCreateView.as_view(), name='category-create'),
     path('product-create/', ProductCreateView.as_view(), name='product-create'),
     path('feedback-create/', FeedbackCreateView.as_view(), name='feedback-create'),
+    path('blog-create/', BlogCreateView.as_view(), name='blog-create'),
 ]
