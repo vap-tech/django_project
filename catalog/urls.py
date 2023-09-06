9fd4c96d2f36e1231b738f83b510d37b684937c7
kot <bafomet2016vitt@gmail.com>
2023-09-06 11:26:13 +0300

    add CRUD to product

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from catalog.views import CategoryListView, CategoryCreateView, ProductListView, \
     ProductCreateView, ProductDetailView, FeedbackCreateView, FeedbackListView, BlogListView, \
-    BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView
+    BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView, ProductDeleteView, ProductUpdateView
 
 from catalog.apps import CatalogConfig
 
@@ -14,10 +14,13 @@ urlpatterns = [
     path('messages/', FeedbackListView.as_view(), name='messages'),
     path('blog/', BlogListView.as_view(), name='blog'),
 
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
     path('blog/<str:slug>/', BlogDetailView.as_view(), name='blog-detail'),
+
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product-update'),
     path('blog/<str:slug>/update/', BlogUpdateView.as_view(), name='blog-update'),
 
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product-delete'),
     path('blog/<str:slug>/delete/', BlogDeleteView.as_view(), name='blog-delete'),
 
     path('category-create/', CategoryCreateView.as_view(), name='category-create'),
