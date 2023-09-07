9da51b041c054016294ae58cb144e75fa11cc4c4
kot <bafomet2016vitt@gmail.com>
2023-09-07 18:42:54 +0300

    add users

---
+++ users/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+from users.models import Country, User
+
+
+class UserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class CountryForm(forms.ModelForm):
+
+    class Meta:
+        model = Country
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from catalog.models import NULLABLE
+
+
+class Country(models.Model):
+    country = models.CharField(max_length=40, verbose_name='страна')
+    description = models.TextField(max_length=200, verbose_name='описание')
+    flag = models.ImageField(upload_to='users/country/', verbose_name='превью', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'Страна'
+        verbose_name_plural = 'Страны'
+
+
+class User(AbstractUser):
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT, verbose_name='страна')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
