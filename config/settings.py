7a94021b01be2858d2966e1c232f55febf33f79c
kot <bafomet2016vitt@gmail.com>
2023-09-08 23:49:12 +0300

    fix login url

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/users/'
 
 EMAIL_HOST = 'mail.v-petrenko.ru'
 EMAIL_PORT = 25
